bcf62fc3609c3b9a3aaf11eb23289580a7673064
keneilwepat <140274289+keneilwepat@users.noreply.github.com>
2024-08-16 12:31:24 +0200

    Add files via upload

---
+++ src/main/java/za/co/protogen/root_project/RootProjectApplication.java
@@ -0,0 +1,13 @@
+package za.co.protogen.root_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/protogen/root_project/RootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.protogen.root_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
